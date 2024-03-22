3d2ff80fafb43410d23d907c341190bd2ea671aa
vwital <wital128@tut.by>
2024-03-22 13:11:49 +0300

    feat: getArgumentsCount solved

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,9 @@ getCurrentFunctionName();
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (!func) return '';
+  return func.toString();
 }
 
 /**
@@ -50,8 +51,10 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const arrOfArgs = [];
+  funcs.forEach((func) => arrOfArgs.push(func.length));
+  return arrOfArgs;
 }
 
 /**
